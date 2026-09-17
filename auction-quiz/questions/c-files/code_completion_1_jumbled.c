230592dda8c22bf5e74ea6efceaec7b9c7a8a5e0
RudraMistry-cmd <mistryrudra51@gmail.com>
2026-09-17 07:38:41 +0530

    feat(questions): add jumbled factorial C file for code_completion_1

---
+++ auction-quiz/questions/c-files/code_completion_1_jumbled.c
@@ -0,0 +1,12 @@
+int n, i;
+#include <stdio.h>
+unsigned long long fact = 1;
+return 0;
+scanf("%d", &n);
+printf("Enter a positive integer: ");
+for (i = 1; i <= n; i++) {
+int main() {
+    fact *= i;
+}
+printf("Factorial = %llu\n", fact);
+}
